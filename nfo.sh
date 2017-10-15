e229e221b027f83fe75b4464228e33afa4648f01
Chatonhub84 <exemple17@gmail.com>
2017-10-15 22:18:14 +0200

    Create nfo.sh

---
+++ nfo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Nom du script .. : nfo.sh
+# Date ........... : 05.2016
+# Version......... : 1.0
+# Auteur ......... : pcccp
+# Description .... : Création NFO automatique
+# Prerequis ...... : mediainfo
+# Execution ...... : "sh nfo.sh"
+
+
+
+ls >>temp.txt
+while read ligne  
+do  
+  mediainfo --LogFile="$ligne".nfo "$ligne"
+done < temp.txt
+rm temp.txt 
+rm temp.txt.nfo
+rm nfo.sh.nfo
